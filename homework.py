b3104ff253d0dadb3776f7741dfa875222bbdfc0
Vladislav Chernyshov <vvv134231979WWW@yandex.ru>
2024-11-23 11:28:50 +0300

    final_version+test_yandex

--- homework.py
+++ homework.py
@@ -1,8 +1,9 @@
-import logging                                         # НЕ ЗАБЫТЬ УБРАТЬ ФАЙЛ С ТОКЕНАМИ ИЗ РЕПОЗИТОРИЯ
+import logging
 import requests
 import time
 from dotenv import load_dotenv
 from telebot import TeleBot
+from logger_homework import logger
 from exceptions import (
     ErrorStatusCode,
     NotKeyHomeworks,
@@ -25,8 +26,6 @@ load_dotenv()
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
-    filename='homework.log',
-    filemode='w'
 )
 
 
@@ -37,7 +36,7 @@ def check_tokens():
        TELEGRAM_CHAT_ID is not None:
         return True
     else:
-        logging.critical('Отсутствие переменных окружения')
+        logger.critical('Отсутствие переменных окружения')
         return False
 
 
@@ -45,21 +44,27 @@ def send_message(bot, message):
     """Отправляет сообщение через бота в Телеграмм"""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-        logging.debug('Сообщение отправлено в Telegram: %s', message)
+        logger.debug('Сообщение отправлено в Telegram: %s', message)
     except Exception as error:
-        logging.error('ошибка при отправке сообщения в Telegram: %s', error)
+        logger.error('ошибка при отправке сообщения в Telegram: %s', error)
 
 
 def get_api_answer(timestamp):
     """Делает GET запрос к API Практикума домашки и"""
     """переводит его из типа json в тип python"""
     try:
-        homework_status = requests.get(ENDPOINT, headers=HEADERS, params=timestamp)
+        homework_status = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=timestamp
+        )
     except requests.RequestException as e:
+        logger.error('Ошибка при запросу к эндпоинту')
         print("Ошибка запроса:", e)
     if homework_status.status_code == 200:
         return homework_status.json()
     elif homework_status.status_code != 200:
+        logger.error('Ошибка доступа к странице')
         raise ErrorStatusCode('Ошибка кода страницы!')
 
 
@@ -78,75 +83,85 @@ def parse_status(homework):
     if 'homework_name' in homework:
         homework_name = homework['homework_name']
     else:
-        raise NotKeyHomeworkName('В ответе от API отсутствует ключ homework_name')
+        logger.error('Отсутствие ключа homework_name в API')
+        raise NotKeyHomeworkName(
+            'В ответе от API отсутствует ключ homework_name'
+        )
     if 'status' in homework:
         verdict = homework['status']
         if verdict in HOMEWORK_VERDICTS:
             verdict = HOMEWORK_VERDICTS[verdict]
-            return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+            return (
+                f'Изменился статус проверки работы "{homework_name}".{verdict}'
+            )
         else:
-            raise NotStatusInHomeworkVerdict('Нет статуса подходящего для документации')
+            logger.error(
+                'Нет статуса домашней работы, который подходит по документации'
+            )
+            raise NotStatusInHomeworkVerdict(
+                'Нет статуса подходящего для документации'
+            )
     else:
+        logger.error('В ответе API отсутствует ключ status')
         raise NotKeyStatus('В ответе от API отсутсвует ключ status')
 
 
-#timestamp = int(time.time())
 def main():
     """Основная логика работы бота."""
+    # Создаём объект бота.
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = 0
     time_marker = {'from_date': timestamp}
+    # Проверяем доступность переменных окружения.
     old_tokens = check_tokens()
     if old_tokens is True:
         pass
+    # Запускаем бесконечный цикл.
     while True:
         old_tokens = check_tokens()
         if old_tokens is True:
             pass
         else:
             break
+        # Делаем запрос на API и проверяем ответ.
         homework_status = get_api_answer(time_marker)
         check_response(homework_status)
-        print(homework_status)
+        # Проверяем в API наличие домашних работ.
+        # Если их нет, делаем передышку и через какое-то время запускаем цикл
+        # заного, где снова проверяем наличие домашней работы.
+        # Как только появится новая домашная работа, цикл пойдёт дальше вниз
+        # по коду с текущим ответом от API.
         if len(homework_status['homeworks']) == 0:
-            logging.debug('Получен пустой список домашних работ')
+            logger.debug('Получен пустой список домашних работ')
             time.sleep(RETRY_PERIOD)
             homework_status = get_api_answer(time_marker)
             continue
-        # print(homework_status)
-        #print(type(homework_status))
-        #homework_status = homework_status
-        #print(homework_status)
-        # print(homework_status['homeworks'])
-    # if len(homework_status['homeworks']) == 0:
-
         status_work = parse_status(homework_status['homeworks'][0])
-        requests_from_the_api = requests.get(ENDPOINT, headers=HEADERS, params=time_marker)  # ДОДЕЛАТЬ ГЛАВНЫЙ СКРИПТ, УБРАТЬ ЛИШНЕЕ, СДЕЛАТЬ ЗАПРОСЫ ЧЕРЕЗ ФУНКЦИИ  !!!!!!
-        response = requests_from_the_api.json()
-        print(response)
-        response = response['homeworks'][0]['status']
-        # print(response)
-        # print(type(response))
-        #print(response.json())
         send_message(bot, status_work)
-
         try:
             status_tokens = check_tokens()
             if status_tokens is True:
                 pass
             else:
                 break
-            new_request = requests.get(ENDPOINT, headers=HEADERS, params=time_marker) # ДОДЕЛАТЬ ГЛАВНЫЙ СКРИПТ, УБРАТЬ ЛИШНЕЕ, СДЕЛАТЬ ЗАПРОСЫ ЧЕРЕЗ ФУНКЦИИ  !!!!!
-            new_response = new_request.json()
-            new_response = new_response['homeworks'][0]['status']
-            if response == new_response:
-                time.sleep(RETRY_PERIOD)
-            elif response != new_response:
-                new_status_work = parse_status(new_response)
+            # Тут мы узнаём, появился ли у домашней работы новый статус за
+            # счёт того что делаем новый запрос. Если в ответе от API пришёл
+            # тот же статус, цикл начнётся заного и через 10 минут повторится
+            # новый запрос к API где мы снова сравним новый статус со статусом
+            # от запроса, который был сделан 10 минут спустя.
+            time.sleep(RETRY_PERIOD)
+            new_request = get_api_answer(time_marker)
+            if homework_status['homeworks'][0]['status'] == \
+                    new_request['homeworks'][0]['status']:
+                logger.debug('В домашней работе отсутствует новый статус')
+            elif homework_status['homeworks'][0]['status'] != \
+                    new_request['homeworks'][0]['status']:
+                new_status_work = parse_status(new_request)
                 send_message(bot, new_status_work)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            logger.error(message)
             send_message(bot, message)
 
 
